f8a9801d3cba070aefe77131ea5adb5c39d25bda
mateuszu94 <109241442+mateuszu94@users.noreply.github.com>
2023-05-03 16:07:55 +0200

    Initial commit

---
+++ bmiCalculator.py
@@ -0,0 +1,25 @@
+name = input("Enter you name: ")
+
+weight = int(input("Enter your weight in Kg: "))
+
+height = int(input("Enter your height in cm: "))
+
+BMI = weight / ((height * height)/10000)
+
+print(BMI)
+
+if BMI>0:
+    if(BMI<18.5):
+        print(name +", you are underwight.")
+    elif (BMI<=24.9):
+        print(name +", you are normal weight.")
+    elif (BMI<29.9):
+        print(name +", you are overweight.")
+    elif (BMI<34.9):
+        print(name +", you are obese.")
+    elif (BMI<39.9):
+        print(name +", you are severely obese.")
+    else:
+        print(name +", you are morbidly obese.")
+else:
+    print("Enter valid input")
